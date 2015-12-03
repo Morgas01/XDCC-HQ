a815582c8f8ed29e98b4f2be59e8f919341d8f2f
Morgas <morgas01@web.de>
2015-12-03 20:08:48 +0100

    added subOffices

---
+++ subOffices/EveTaku_Ririchiyo.js
@@ -0,0 +1,6 @@
+exports = module.exports = require("../mercenaries/xdccList");
+
+exports.network="irc.rizon.net";
+exports.channel="#evetaku";
+exports.url="http://www.evetaku.com:8000";
+exports.bot="EveTaku|Ririchiyo";
---
+++ subOffices/[XDCC]Botan.js
@@ -0,0 +1,6 @@
+exports = module.exports = require("../mercenaries/ircOffer");
+
+exports.network="irc.rizon.net";
+exports.channel="#evetaku";
+exports.url="http://kyou.x1n.pl:8000";
+exports.bot="[XDCC]Botan";
