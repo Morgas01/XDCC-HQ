990057d6964a03a005c8d7e58e68dbbfa0497bd1
Morgas <morgas01@web.de>
2016-01-20 18:24:33 +0100

    ircOverview

--- webapp/js/ircOverview.js
+++ webapp/js/ircOverview.js
@@ -6,6 +6,7 @@
 		rq:"request"
 	});
 	
+	
 	/**** Tabs ****/
 	
 	var systemTab=new SC.Tab("system");
@@ -15,6 +16,7 @@
 
 	
 	/**** commands ****/
+	
 	var commands={
 		"connect":url=>({path:"connect",url:url}),
 		"join":channel=>({path:"join",url:tabContainer.activeTab.server,channel:channel}),
@@ -46,6 +48,7 @@
 				}
 				if(command===null)return e.target.setCustomValidity("no such command");
 			}
+			else command="msg";
 			var data=commands[command](line);
 			SC.rq({url:"rest/irc/"+data.path,data:JSON.stringify(data)});
 			event.target.value="";
@@ -53,9 +56,39 @@
 	},false);
 	
 	
+	/**** actions ****/
+	
+	var actions={
+		"join":d=>SC.rq({url:"rest/irc/join",data:JSON.stringify({
+			url:tabContainer.activeTab.server,
+			channel:d.channel
+		})}),
+		"addDownload":d=>SC.rq({url:"rest/download/add",data:JSON.stringify([{
+			network:tabContainer.activeTab.server,
+			channel:tabContainer.activeTab.target,
+			bot:d.bot,
+			packnumber:d.packnumber
+		}])})
+	}
+	tabContainer.domElement.addEventListener("click",function(e)
+	{
+		if(e.target.tagName==="BUTTON"&&e.target.dataset.action)
+		{
+			if(e.target.dataset.action in actions)
+				actions[e.target.dataset.action](e.target.dataset);
+			else alert("no such action :"+e.target.dataset.action);
+		}
+	})
+	
+	
 	/**** Messages ****/
 	
+	var ircColor=/\x03(\d\d)(?:,(\d\d))?([^\x03]+)\x03/g;
+	var ircBold=/\x02([^\x02]+)\x02/g;
+	var ircItalic=/\x1D([^\x1D]+)\x1D/g;
+	var ircUnderline=/\x1F([^\x1F]+)\x1F/g;
 	var urlRegEx=/([a-z]{3,6}?:\/\/)?([a-z0-9\-]+\.)+[a-z]{2,6}(:\d+)?[\.\?\=\&\%\/\w\-]*\b([^@]|$)/ig;
+	var downloadRegEx=/\/msg (\S+) xdcc send (\d+)/ig;
 	
 	var es=new EventSource("rest/irc/list");
 	es.addEventListener("error",µ.logger.error);
@@ -96,21 +129,21 @@
 		row.appendChild(text);
 		tab.content.appendChild(row);
 	};
-	var ircColor=/\x03(\d\d)(?:,(\d\d))?([^\x03]+)\x03/g;
-	var ircBold=/\x02([^\x02]+)\x02/g;
-	var ircItalic=/\x1D([^\x1D]+)\x1D/g;
-	var ircUnderline=/\x1F([^\x1F]+)\x1F/g;
+	
 	var tranformText=function(text)
 	{
 		text=text.replace(ircColor,'<span class="front-$1 back-$2">$3</span>');
 		text=text.replace(ircBold,'<b>$1</b>');
 		text=text.replace(ircItalic,'<i>$1</i>');
 		text=text.replace(ircUnderline,'<u>$1</u>');
-		//text=text.replace(/\n/g,"<br>");
+		text=text.replace(downloadRegEx,'<button data-action="addDownload" data-bot="$1" data-packnumber="$2">add download</button>');
 		text=text.replace(urlRegEx,t=>'<a target="_blank" href="'+t+'">'+t+'</a>');
-		text=text.replace(/([#$]\w+)/g,'<button data-channel="$1">$1</button>');
+		text=text.replace(/([#$]\w+)/g,'<button data-action="join" data-channel="$1">$1</button>');
 		return text;
 	}
+	
+	
+	/**** Data ****/
 	
 	es.addEventListener("list",function onList (listEvent)
 	{
