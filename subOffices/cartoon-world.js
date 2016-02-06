5ddd53fabe3a2bbf615cb77ad136d69d86c34694
Morgas <morgas01@web.de>
2016-02-06 22:17:21 +0100

    new suboffice

---
+++ subOffices/cartoon-world.js
@@ -0,0 +1,5 @@
+exports = module.exports = require("../mercenaries/xdccParser");
+
+exports.network="irc.xertion.org";
+exports.channel="#cartoon-world";
+exports.url="http://cartoon-world.org/xdcc";
