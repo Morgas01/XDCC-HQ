c634bd3bda0f3b638997395551bf612398093a04
Morgas <morgas01@web.de>
2017-04-10 13:34:58 +0200

    subOffices

---
+++ subOffices/SaberLily.js
@@ -0,0 +1,6 @@
+exports = module.exports = require("../mercenaries/ircOffer");
+
+exports.network="irc.rizon.net";
+exports.channel="#mori";
+exports.url="http://62.113.226.226:9001";
+exports.bot="SaberLily";
---
+++ subOffices/animesenshi.js
@@ -0,0 +1,5 @@
+exports = module.exports = require("../mercenaries/xdccParser");
+
+exports.network="irc.rizon.net";
+exports.channel="#animesenshi";
+exports.url="http://asenshi.moe/xdcc";
