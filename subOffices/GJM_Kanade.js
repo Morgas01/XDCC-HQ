69222de7f17804cbe0137496a9086952ec92e008
Morgas <morgas01@web.de>
2017-08-11 16:56:24 +0200

    subOffices

---
+++ subOffices/GJM_Kanade.js
@@ -0,0 +1,2 @@
+exports = module.exports = require("../mercenaries/xdccList")
+("irc.rizon.net","#goodjob","http://i.tzn.re/kanade.txt");
---
+++ subOffices/chihiro.js
@@ -0,0 +1,2 @@
+exports = module.exports = require("../mercenaries/xdccParser")
+("irc.rizon.net","#chihiro","http://xdcc.chihiro-subs.us/");
