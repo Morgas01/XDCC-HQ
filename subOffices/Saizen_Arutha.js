f7fdf75900f26ddced54137f9514945b833408b7
Morgas <morgas01@web.de>
2018-02-18 23:30:25 +0100

    subOffice

---
+++ subOffices/Saizen_Arutha.js
@@ -0,0 +1,2 @@
+exports = module.exports = require("../mercenaries/xdccList")
+("irc.rizon.net","#saizen","http://arutha.info:4700/txt","Saizen|Arutha");
---
+++ subOffices/Saizen_Mikasa.js
@@ -0,0 +1,2 @@
+exports = module.exports = require("../mercenaries/xdccList")
+("irc.rizon.net","#saizen","http://koa-fansubs.com/global/Lists/Saizen%7CMikasa.txt","Saizen|Mikasa");
