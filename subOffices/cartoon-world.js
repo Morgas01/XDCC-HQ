822f4f4bb313385ad2dd1df7bc99c14b6bc0c504
Morgas <morgas01@web.de>
2019-11-18 10:09:17 +0100

    fix url

--- subOffices/cartoon-world.js
+++ subOffices/cartoon-world.js
@@ -1,2 +1,2 @@
 exports = module.exports = require("../mercenaries/xdccParser")
-("irc.xertion.org","#cartoon-world","http://cartoon-world.org/xdcc");
+("irc.xertion.org","#cartoon-world","https://cartoon-world.org/xdcc");
